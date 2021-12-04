adb943b8e1c9c40d0f028bd5623c6de2af67cf9e
Ainun <ayi.jayz13@gmail.com>
2021-12-04 12:39:19 +0800

    Try GridLayout with RecyclerView

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -37,14 +37,14 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
 
     class ViewHolder private constructor(itemView: View) :
         RecyclerView.ViewHolder(itemView) {
-        val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
+//        val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
         val quality: TextView = itemView.findViewById(R.id.quality_string)
         val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
         fun bind(item: SleepNight) {
             val res = itemView.context.resources
-            sleepLength.text = convertDurationToFormatted(
-                item.startTimeMilli, item.endTimeMilli, res
-            )
+//            sleepLength.text = convertDurationToFormatted(
+//                item.startTimeMilli, item.endTimeMilli, res
+//            )
             quality.text = convertNumericQualityToString(
                 item.sleepQuality, res
             )
